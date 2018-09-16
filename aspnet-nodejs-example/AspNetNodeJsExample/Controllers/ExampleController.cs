83ece7bc3984901139797dccda3bc25e9ef5198a
Michał Michalczuk <michalczukm@gmail.com>
2018-09-17 00:44:32 +0200

    added another saple nodejs script to aspnet app

--- aspnet-nodejs-example/AspNetNodeJsExample/Controllers/ExampleController.cs
+++ aspnet-nodejs-example/AspNetNodeJsExample/Controllers/ExampleController.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.NodeServices;
@@ -9,17 +10,17 @@ namespace AspNetNodeJsExample.Controllers
     [ApiController]
     public class ExampleController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<dynamic> Get()
-        {
-            return new {foo = 51};
-        }
-
         [HttpPost("pdf")]
-        public async Task<FileStreamResult> GetPdf([FromBody] string html, [FromServices] INodeServices nodeServices)
+        public async Task<FileStreamResult> ConvertToPdf([FromBody] string html, [FromServices] INodeServices nodeServices)
         {
             var stream = await nodeServices.InvokeAsync<Stream>("NodeScripts/converter", html);
             return File(stream, "application/pdf");
+        }
+
+        [HttpGet("quote")]
+        public async Task<dynamic> GetQuote([FromQuery] string author, [FromServices] INodeServices nodeServices)
+        {
+            return await nodeServices.InvokeExportAsync<dynamic>("NodeScripts/utils", "quote", author);
         }
     }
 }
---
+++ aspnet-nodejs-example/AspNetNodeJsExample/NodeScripts/utils.js
@@ -0,0 +1,9 @@
+﻿const randomQuotes = require('random-quotes');
+
+const getQuote = (author) => author
+    ? randomQuotes.byAuthor(author)
+    : randomQuotes.default();
+
+module.exports = {
+    quote: (callback, author) => callback(null, getQuote(author))
+}
