8d4d79dde01f974b9245bab1e13ae7ba9e37131e
Michał Michalczuk <michalczukm@gmail.com>
2019-04-16 00:30:56 +0200

    minor improvements

--- aspnet-nodejs-example/AspNetNodeJsExample/Controllers/ExampleController.cs
+++ aspnet-nodejs-example/AspNetNodeJsExample/Controllers/ExampleController.cs
@@ -11,14 +11,18 @@ namespace AspNetNodeJsExample.Controllers
     public class ExampleController : ControllerBase
     {
         [HttpPost("pdf")]
-        public async Task<FileStreamResult> ConvertToPdf([FromBody] string html, [FromServices] INodeServices nodeServices)
+        public async Task<FileStreamResult> ConvertToPdf(
+            [FromBody] string html,
+            [FromServices] INodeServices nodeServices)
         {
             var stream = await nodeServices.InvokeAsync<Stream>("NodeScripts/converter", html);
             return File(stream, "application/pdf");
         }
 
         [HttpGet("quote")]
-        public async Task<dynamic> GetQuote([FromQuery] string author, [FromServices] INodeServices nodeServices)
+        public async Task<dynamic> GetQuote(
+            [FromQuery] string author, 
+            [FromServices] INodeServices nodeServices)
         {
             return await nodeServices.InvokeExportAsync<dynamic>("NodeScripts/utils", "quote", author);
         }
